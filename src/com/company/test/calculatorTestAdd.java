2f7e2c8b18e33443a2ec238e7e5a838fb26d390e
Ralph <raftsdev@gmail.com>
2022-07-05 16:52:06 -0400

    Feat: Add @DisplayName and @Disabled

--- src/com/company/test/calculatorTestAdd.java
+++ src/com/company/test/calculatorTestAdd.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.company.app.Calculator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 
 class calculatorTestAdd {
 
@@ -10,16 +12,19 @@ class calculatorTestAdd {
     @BeforeAll
     static void setup() {
         System.out.println("********* Start test of Add operation ***********");
-        
+        //var calc = new Calculator();
+
     }
 
     @BeforeEach
     void init() {
         count++;
         System.out.println("Test N"+count);
+        //var calc = new Calculator();
     }
 
     @org.junit.jupiter.api.Test
+    @DisplayName("All numbers positive")
     void add() {
         var calc = new Calculator();
         assertEquals(5, calc.add(3,2));
@@ -32,6 +37,7 @@ class calculatorTestAdd {
     }
 
     @org.junit.jupiter.api.Test
+    @Disabled("Not implemented yet")
     void addAllNegative() {
         var calc = new Calculator();
         assertEquals(-5, calc.add(-3,-2));
